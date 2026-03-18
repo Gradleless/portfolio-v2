96139acd043fd8dbcbd2dde060e8763108446dfb
Lyciaa <57102194+Gradleless@users.noreply.github.com>
2026-03-18 09:00:29 +0100

    refactor(sitemap): extract urlEntry helper to clean up template literals

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,36 +1,33 @@
 import { getAllPosts } from '$lib/utils/blog';
 
 export const prerender = true;
-const website = 'https://lycia.dev';
+
+const BASE_URL = 'https://lycia.dev';
+
+function urlEntry(loc: string, priority: number, changefreq: string, lastmod: string) {
+	return `  <url>
+    <loc>${loc}</loc>
+    <changefreq>${changefreq}</changefreq>
+    <priority>${priority}</priority>
+    <lastmod>${lastmod}</lastmod>
+  </url>`;
+}
 
 export async function GET() {
 	const posts = await getAllPosts();
-	
+	const buildDate = new Date().toISOString();
+
+	const urls = [
+		urlEntry(BASE_URL, 1.0, 'monthly', buildDate),
+		urlEntry(`${BASE_URL}/blog`, 0.8, 'weekly', buildDate),
+		...posts.map((post) =>
+			urlEntry(`${BASE_URL}/blog/${post.slug}`, 0.7, 'monthly', new Date(post.date).toISOString())
+		)
+	];
+
 	const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-	<url>
-		<loc>${website}</loc>
-		<changefreq>monthly</changefreq>
-		<priority>1.0</priority>
-		<lastmod>${new Date().toISOString()}</lastmod>
-	</url>
-	<url>
-		<loc>${website}/blog</loc>
-		<changefreq>weekly</changefreq>
-		<priority>0.8</priority>
-		<lastmod>${new Date().toISOString()}</lastmod>
-	</url>
-	${posts
-		.map(
-			(post) => `
-	<url>
-		<loc>${website}/blog/${post.slug}</loc>
-		<changefreq>monthly</changefreq>
-		<priority>0.7</priority>
-		<lastmod>${new Date(post.date).toISOString()}</lastmod>
-	</url>`
-		)
-		.join('')}
+${urls.join('\n')}
 </urlset>`;
 
 	return new Response(sitemap, {
